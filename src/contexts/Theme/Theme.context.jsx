2a7f82095c44cee906ce23590415ba60ffed3c4e
Michal <michal.front@init.de>
2023-09-19 17:04:13 +0200

    add first theme option

---
+++ src/contexts/Theme/Theme.context.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, useContext } from "react";
+export const ThemeContext = createContext({});
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
