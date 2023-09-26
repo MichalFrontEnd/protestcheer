4c517781ca22cccefdd6f43d814038228c9b7168
Michal <michal.front@init.de>
2023-09-26 15:41:43 +0200

    Adds localstorage saving and cleans up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import { useState } from "react";
 import "./App.scss";
 import { ThemeToggle } from "./components/molecules/themetoggle";
 
@@ -8,20 +6,6 @@ function App() {
     <div className={`theme-context`}>
       <div className="App">
         <ThemeToggle />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     </div>
   );
