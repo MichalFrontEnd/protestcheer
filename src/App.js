654adede6471e1428ce6e81725664d60c813e6b4
Michal <michal.front@init.de>
2023-09-27 12:06:55 +0200

    Adds header for toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.scss";
-import { Footer, CarouselElement } from "./components/organisms";
-import { ThemeToggle } from "./components/molecules";
+import { Header, Footer, CarouselElement } from "./components/organisms";
 
 //ToDo: Decide if toggle switch stays here
 function App() {
   return (
     <main className="theme-context">
       <div className="container">
-        <ThemeToggle />
+        <Header />
         <CarouselElement />
         <Footer />
       </div>
---
+++ src/components/organisms/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ThemeToggle } from "../../molecules";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <ThemeToggle />
+    </div>
+  );
+};
+
+export default Header;
